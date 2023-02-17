579604168e8ecf8e3493ab8397fbbfed5c671b72
DmitriyPodkovko <dapodkovko@gmail.com>
2023-02-17 15:08:46 +0200

    del drafts, add db/

--- const/constants.py
+++ const/constants.py
@@ -1,4 +1,5 @@
 IPINFO_URL = 'https://ipinfo.io/'
+# for example: iter(('', '?token=token1', '?token=token2', '?token=token3'))
 ITERATOR_TOKENS = iter(('', '', '', '', ''))
 IN_FILE = 'example.csv'
 SEPARATOR = ';'
@@ -11,16 +12,3 @@ DB_NAME = 'ip_resolved_list.db'
 # how many days to consider the 'whois' field is out of date
 # and need to updated at the time of the request
 LIMIT_DAY = 5
-QUERIES = dict(
-    CREATE_TABLE="""
-        CREATE TABLE IF NOT EXISTS ip (
-        ip_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        ip TEXT NOT NULL UNIQUE,
-        whois TEXT NOT NULL,
-        created_at TEXT DEFAULT CURRENT_TIMESTAMP,
-        update_at TEXT DEFAULT CURRENT_TIMESTAMP)
-    """,
-    GET_IP="""SELECT * FROM ip WHERE ip=?""",
-    INSERT_IP="""INSERT INTO ip(ip, whois) VALUES(?,?)""",
-    UPDATE_IP="""UPDATE ip SET whois = ?, update_at = ? WHERE ip=?"""
-)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Decoding the organization's IP address from csv using ipinfo
+# use nmap - optional
 import nmap3
 from sys import exit
 from pandas import read_csv
@@ -6,16 +7,13 @@ from sqlite3 import connect
 from requests import get
 from numpy import nan
 from datetime import datetime, timedelta
-from const.constants import (IPINFO_URL, ITERATOR_TOKENS,
-                             IN_FILE, SEPARATOR, NMAP,
-                             OUT_FILE_ALL_IP, OUT_FILE_UKR_IP,
-                             OUT_FILE_UKR_MOBILE_IP,
-                             DB_NAME, LIMIT_DAY, QUERIES as q)
+from const import constants as c
+from db import queries as q
 
 
 def get_next_token():
     try:
-        token = next(ITERATOR_TOKENS, '-1')
+        token = next(c.ITERATOR_TOKENS, '-1')
         if token == '-1':
             exit('TOKENS ARE ENDED !!!')
         print('Run ' + token)
@@ -32,8 +30,6 @@ def get_response(url, ip_address, token):
             result = (f"{response.json()['city'] if 'city' in response.json() else 'No City'}, "
                       f"{response.json()['country'] if 'country' in response.json() else 'No Country'}, "
                       f"{response.json()['org'] if 'org' in response.json() else 'No Organisation'}")
-            if type(result) is tuple:
-                result = result[0]
             return result, token
         elif response.status_code == 429:
             token = get_next_token()
@@ -83,7 +79,7 @@ def handler(input_file, delimiter, url, token, output_file_all_ip, output_file_u
         df = read_csv(input_file, delimiter=delimiter, index_col='ID')
         ip_list = list(df.get('IP Address'.strip()))
         result_list = []
-        if NMAP == 1:
+        if c.NMAP == 1:
             nmap_list = []
             nmap_dict = {}
         count_resp_db = 0
@@ -91,44 +87,47 @@ def handler(input_file, delimiter, url, token, output_file_all_ip, output_file_u
         count_insert_db = 0
         count_update_db = 0
         try:
-            with connect(DB_NAME) as conn:
+            with connect(c.DB_NAME) as conn:
                 cur = conn.cursor()
-                cur.execute(q['CREATE_TABLE'])
+                cur.execute(q.QUERIES['CREATE_TABLE'])  # IF NOT EXISTS
                 for ip in range(0, len(ip_list)):
                     if ip_list[ip] is not nan:
                         if str(ip_list[ip]).strip():
-                            cur.execute(q['GET_IP'], (ip_list[ip],))
+                            # Select from {DB_NAME}.db
+                            cur.execute(q.QUERIES['GET_IP'], (ip_list[ip],))
                             row = cur.fetchone()
-                            # check if IP exists in DB
+                            # check if IP exists in {DB_NAME}.db
                             if row is None:
-                                # IP absent then make a request and insert result into DB
+                                # IP absent then make a request and insert result into {DB_NAME}.db
                                 whois, token = get_response(url, ip_list[ip], token)
+                                if type(whois) is tuple:
+                                    whois = whois[0]
                                 count_resp_ipinfo += 1
                                 result_list.append(whois)
-                                cur.execute(q['INSERT_IP'], (ip_list[ip], whois,))
+                                cur.execute(q.QUERIES['INSERT_IP'], (ip_list[ip], whois,))
                                 conn.commit()
                                 count_insert_db += 1
                             else:
-                                # IP exist in DB
+                                # IP exist in {DB_NAME}.db
                                 # get date now and calculate out of date (limit_date)
                                 db_update_at = datetime.strptime(row[4], '%Y-%m-%d %H:%M:%S')
                                 today = datetime.now().replace(microsecond=0)
-                                limit_date = today - timedelta(days=LIMIT_DAY)
-                                # if IP date in DB is out of date
-                                # then make a request and update IP date in DB
+                                limit_date = today - timedelta(days=c.LIMIT_DAY)
+                                # if IP date in {DB_NAME}.db is out of date
+                                # then make a request and update IP date in {DB_NAME}.db
                                 if db_update_at < limit_date:
                                     whois, token = get_response(url, ip_list[ip], token)
                                     count_resp_ipinfo += 1
                                     result_list.append(whois)
-                                    cur.execute(q['UPDATE_IP'], (whois, today, ip_list[ip],))
+                                    cur.execute(q.QUERIES['UPDATE_IP'], (whois, today, ip_list[ip],))
                                     conn.commit()
                                     count_update_db += 1
                                 else:
-                                    # IP date in DB is fresh
+                                    # IP date in {DB_NAME}.db is fresh
                                     whois = row[2]
                                     result_list.append(whois)
                                     count_resp_db += 1
-                            if NMAP == 1:
+                            if c.NMAP == 1:
                                 if ip_list[ip] in nmap_dict:
                                     nmap_list.append(nmap_dict.get(ip_list[ip]))
                                 else:
@@ -137,16 +136,16 @@ def handler(input_file, delimiter, url, token, output_file_all_ip, output_file_u
                                     nmap_list.append(ip_nmap)
                         else:  # value of ip_list[ip] is empty
                             result_list.append('')
-                            if NMAP == 1:
+                            if c.NMAP == 1:
                                 nmap_list.append('')
                     else:  # value of ip_list[ip] is nan
                         result_list.append('')
-                        if NMAP == 1:
+                        if c.NMAP == 1:
                             nmap_list.append('')
         except Exception as e_db:
             print(f'DB error: {str(e_db)}')
         df.insert(11, 'Organisation', result_list)
-        if NMAP == 1:
+        if c.NMAP == 1:
             df.insert(12, 'Nmap', nmap_list)
         # -sig is necessary so that the Cyrillic alphabet does not move out and not on Apple (Windows, Android)
         df.to_csv(output_file_all_ip, sep=delimiter, index=True, header=True, encoding='utf-8-sig')
@@ -157,7 +156,7 @@ def handler(input_file, delimiter, url, token, output_file_all_ip, output_file_u
         df_ukr_ip.to_csv(output_file_ukr_ip, sep=delimiter, index=True, header=True, encoding='utf-8-sig')
         df_ukr_mobile_ip.to_csv(output_file_ukr_mobile_ip, sep=delimiter, index=True, header=True, encoding='utf-8-sig')
         print(f'Number of responses from the database: {count_resp_db}')
-        print(f'Number of responses from {IPINFO_URL}: {count_resp_ipinfo}')
+        print(f'Number of responses from {c.IPINFO_URL}: {count_resp_ipinfo}')
         print(f'Number of ip inserted into the database: {count_insert_db}')
         print(f'Number of ip updated in the database: {count_update_db}')
     except Exception as e_csv:
@@ -166,6 +165,6 @@ def handler(input_file, delimiter, url, token, output_file_all_ip, output_file_u
 
 if __name__ == '__main__':
     tkn = get_next_token()
-    if LIMIT_DAY == 0:
-        LIMIT_DAY = 1
-    handler(IN_FILE, SEPARATOR, IPINFO_URL, tkn, OUT_FILE_ALL_IP, OUT_FILE_UKR_IP, OUT_FILE_UKR_MOBILE_IP)
+    if c.LIMIT_DAY == 0:
+        c.LIMIT_DAY = 1
+    handler(c.IN_FILE, c.SEPARATOR, c.IPINFO_URL, tkn, c.OUT_FILE_ALL_IP, c.OUT_FILE_UKR_IP, c.OUT_FILE_UKR_MOBILE_IP)
